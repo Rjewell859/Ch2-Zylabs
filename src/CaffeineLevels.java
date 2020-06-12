37ad07a645e4d76b5894c18e7571118eabb4b49b
GlaDOS <hardcorepunkguitar@gmail.com>
2020-06-12 15:05:11 -0400

    CaffeineLevels is all set.

--- src/CaffeineLevels.java
+++ src/CaffeineLevels.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class CaffeineLevels {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
-        double caffeineMg; // "double" supports floating-point like 75.5, versus int for integers like 75.
+        System.out.print("This program determines the half life of caffeine, enter a dose in milligrams: ");
+
+        double currentMg;
+        double caffeineMg;
 
         caffeineMg = scnr.nextDouble();
-        System.out.print(caffeineMg);
+        currentMg = caffeineMg / 2;
+
+        System.out.printf("After 6 hours: %.2f mg\n", + currentMg);
+        currentMg = currentMg / 2;
+
+        System.out.printf("After 12 hours: %.2f mg\n", + currentMg);
+        currentMg = currentMg / 4;
+
+        System.out.printf("After 24 hours: %.2f mg\n", + currentMg);
 
     }
 }
