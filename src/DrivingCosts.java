a9143e079ef3304f2ede7d5e42f24f1c30ca0be9
GlaDOS <hardcorepunkguitar@gmail.com>
2020-06-13 16:36:43 -0400

    DrivingCosts completed.

--- src/DrivingCosts.java
+++ src/DrivingCosts.java
@@ -2,6 +2,21 @@ import java.util.Scanner;
 
 public class DrivingCosts {
     public static void main(String[] args) {
-        /* Type your code here. */
+        Scanner scnr = new Scanner(System.in);
+        double mileGallon = scnr.nextDouble();
+        double dollarGallon = scnr.nextDouble();
+        double dollarMile = dollarGallon/mileGallon;
+        int distTravel = 20;
+
+        System.out.printf("%.2f", (dollarMile)*distTravel);
+        System.out.print(" ");
+        distTravel = 75;
+        System.out.printf("%.2f", (dollarMile)*distTravel);
+        System.out.print(" ");
+        distTravel = 500;
+        System.out.printf("%.2f", (dollarMile)*distTravel);
+        System.out.println();
     }
+
+
 }
